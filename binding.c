8f485606b3831d69eb28443ac97e9470bd81e403
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-06 17:03:33 +0100

    Handle missing Windows values

--- binding.c
+++ binding.c
@@ -250,14 +250,14 @@ bare_os_user_info(js_env_t *env, js_callback_info_t *info) {
   assert(err == 0);
 
   js_value_t *uid;
-  err = js_create_int64(env, pwd.uid, &uid);
+  err = js_create_int64(env, pwd.uid == (typeof(pwd.uid)) -1 ? -1 : pwd.uid, &uid);
   assert(err == 0);
 
   err = js_set_named_property(env, result, "uid", uid);
   assert(err == 0);
 
   js_value_t *gid;
-  err = js_create_int64(env, pwd.gid, &gid);
+  err = js_create_int64(env, pwd.gid == (typeof(pwd.gid)) -1 ? -1 : pwd.gid, &gid);
   assert(err == 0);
 
   err = js_set_named_property(env, result, "gid", gid);
