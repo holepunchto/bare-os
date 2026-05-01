b96fcb5fa110dcd81223365121f3e7d9ec4e1aa7
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-01 11:05:35 +0200

    Formatting

--- lib/errors.d.ts
+++ lib/errors.d.ts
@@ -1,11 +1,5 @@
 declare class OSError extends Error {
-  constructor(msg: string, code: string, fn?: Function)
-
   readonly code: string
-  readonly name: 'OSError'
-
-  static UNKNOWN_SIGNAL(msg: string): OSError
-  static TITLE_OVERFLOW(msg: string): OSError
 }
 
 export = OSError
